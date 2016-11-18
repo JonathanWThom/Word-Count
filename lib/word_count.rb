da12303ba78b4b9de25f49081233ab5d108c30c6
Jonathan Thom <jonathan.thom1990@gmail.com>
2016-11-18 10:55:58 -0800

    Further refactor method

--- lib/word_count.rb
+++ lib/word_count.rb
@@ -2,8 +2,7 @@ class String
   define_method(:word_count) do |string, option|
     counter = 0
     if option == 'full'
-      words = string.downcase.split(" ").each {|word| word.gsub!(/[^0-9A-Za-z]/, '')}
-      counter = words.count(self.downcase)
+      counter = string.downcase.split(" ").each {|word| word.gsub!(/[^0-9A-Za-z]/, '')}.count(self.downcase)
     elsif option == 'partial'
       counter = string.downcase.scan(/(?=#{self.downcase})/).count
     end
