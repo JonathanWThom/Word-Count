a4063d12247d4b4b0c5a1ac81395ef65ff01d105
Jonathan Thom <jonathan.thom1990@gmail.com>
2016-11-18 08:59:38 -0800

    Refactor method to account for both full and partial word options

--- lib/word_count.rb
+++ lib/word_count.rb
@@ -1,25 +1,33 @@
 class String
-  define_method(:word_count) do |string|
+  define_method(:word_count) do |string, option|
     counter = 0
     words = string.downcase.split(" ")
-    words.each() do |word|
-      if word == self.downcase
-        counter += 1
+    if option == "full"
+      words.each() do |word|
+        if word == self.downcase
+          counter += 1
+        end
+      end
+    elsif option == "partial"
+      words.each() do |word|
+        if word.include?(self.downcase)
+          counter += 1
+        end
       end
     end
     counter
   end
 end
 
-class String
-  define_method(:word_count_advanced) do |string|
-    counter = 0
-    words = string.downcase.split(" ")
-    words.each() do |word|
-      if word.include?(self.downcase)
-        counter += 1
-      end
-    end
-    counter
-  end
-end
+# class String
+#   define_method(:word_count_advanced) do |string|
+#     counter = 0
+#     words = string.downcase.split(" ")
+#     words.each() do |word|
+#       if word.include?(self.downcase)
+#         counter += 1
+#       end
+#     end
+#     counter
+#   end
+# end
--- spec/word_count_spec.rb
+++ spec/word_count_spec.rb
@@ -3,21 +3,25 @@ require('word_count')
 require('pry')
 
 describe('String#word_count') do
-  it('User can input a target word and a string, and will be returned how many times the target word appears in the string as a full word') do
-    expect('peck'.word_count('If Peter Piper picked a peck of pickled peppers, where\'s the peck of pickled peppers Peter Piper picked?')).to(eq(2))
+  it('User can input a target word and a string, and will be returned how many times the target word appears in the string as a full word (when that option is selected)') do
+    expect('peck'.word_count('If Peter Piper picked a peck of pickled peppers, where\'s the peck of pickled peppers Peter Piper picked?', 'full')).to(eq(2))
   end
 
-  it('User can input a target word or string in either upper or lowercase (or a mix) and and will be returned how many times the target word appears in the string as a full word') do
-    expect('peCk'.word_count('If Peter Piper picked a Peck of pickled peppers, where\'s the pecK of pickled peppers Peter Piper picked?')).to(eq(2))
+  it('User can input a target word and a string, and will be returned how many times the target word appears in the string as a full word OR part of a word (when that option is selected).') do
+    expect('cat'.word_count('I\'m taking my cat on a walk through the catacombs.', 'partial')).to(eq(2))
+  end
+
+  it('User can enter either field in upper or lower case, or a mix') do
+    expect('peCk'.word_count('If Peter Piper picked a Peck of pickled peppers, where\'s the pecK of pickled peppers Peter Piper picked?', 'full')).to(eq(2))
   end
 end
 
-describe('String#word_count_advanced') do
-  it('User can input a target word and a string, and will be returned how many times the target word appears in the string as a full word OR part of a word.') do
-    expect('cat'.word_count_advanced('I\'m taking my cat on a walk through the catacombs.')).to(eq(2))
-  end
-
-  it('User can input a target word or string in either upper or lowercase (or a mix) and and will be returned how many times the target word appears in the string as a full word OR part of a word') do
-    expect('cAT'.word_count_advanced('I\'m taking my cAt on a walk through the cATAcombs')).to(eq(2))
-  end
-end
+# describe('String#word_count_advanced') do
+#   it('User can input a target word and a string, and will be returned how many times the target word appears in the string as a full word OR part of a word.') do
+#     expect('cat'.word_count_advanced('I\'m taking my cat on a walk through the catacombs.')).to(eq(2))
+#   end
+#
+#   it('User can input a target word or string in either upper or lowercase (or a mix) and and will be returned how many times the target word appears in the string as a full word OR part of a word') do
+#     expect('cAT'.word_count_advanced('I\'m taking my cAt on a walk through the cATAcombs')).to(eq(2))
+#   end
+# end
