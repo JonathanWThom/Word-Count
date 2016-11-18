989175b29ea79ce7007b0c79059bffa3042718db
Jonathan Thom <jonathan.thom1990@gmail.com>
2016-11-18 08:23:40 -0800

    Allow users to receive word count when target word is just part of another word

--- lib/word_count.rb
+++ lib/word_count.rb
@@ -10,3 +10,16 @@ class String
     counter
   end
 end
+
+class String
+  define_method(:word_count_advanced) do |string|
+    counter = 0
+    words = string.split(" ")
+    words.each() do |word|
+      if word.include?(self)
+        counter += 1
+      end
+    end
+    counter
+  end
+end
--- spec/word_count_spec.rb
+++ spec/word_count_spec.rb
@@ -7,3 +7,9 @@ describe('String#word_count') do
     expect('peck'.word_count('If Peter Piper picked a peck of pickled peppers, where\'s the peck of pickled peppers Peter Piper picked?')).to(eq(2))
   end
 end
+
+describe('String#word_count_advanced') do
+  it('User can input a target word and a string, and will be returned how many times the target word appears in the string as a full word OR part of a word.') do
+    expect('cat'.word_count_advanced('I\'m taking my cat on a walk through the catacombs.')).to(eq(2))
+  end
+end
